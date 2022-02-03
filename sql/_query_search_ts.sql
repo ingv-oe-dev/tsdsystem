cdbe57a05ce44dcd5a47677561a68cf23b279232
carmelocassisi <carmelocassisi@gmail.com>
2022-02-03 16:39:41 +0100

    updated sql

--- sql/_query_search_ts.sql
+++ sql/_query_search_ts.sql
@@ -8,24 +8,12 @@ where c.id = 0
 -- Search by sensor
 select t.id, t.schema, t.name, 'by_sensor' as search_type 
 from tsd_pnet.sensors s 
-inner join tsd_main.timeseries_mapping_sensors tms on s.id = tms.sensor_id
-inner join tsd_main.timeseries t on tms.timeseries_id = t.id
-where s.id = 0
-
-union 
-
-select t.id, t.schema, t.name, 'by_sensor_channel' as search_type 
-from tsd_pnet.sensors s 
-inner join tsd_pnet.channels c on s.id = c.sensor_id 
+inner join tsd_pnet.channels c on s.id = c.sensor_id
 inner join tsd_main.timeseries_mapping_channels tmc on c.id = tmc.channel_id 
 inner join tsd_main.timeseries t on tmc.timeseries_id = t.id
 where s.id = 0
 
-
-
-
-
--- mapping from timeseries to nets
+-- mapping dependencies from timeseries to nets
 select
 	tmc.timeseries_id, tmc.channel_id, c.sensor_id, s.net_id 
 from
