dadeeabdc8a8f0e96271ef82c9406711e70447bd
carmelocassisi <carmelocassisi@gmail.com>
2022-04-11 14:37:49 +0200

    Added create table tsd_users.temp_reset_keys

--- docker/timescale/sql/tsd_users.sql
+++ docker/timescale/sql/tsd_users.sql
@@ -111,3 +111,19 @@ CREATE TABLE IF NOT EXISTS tsd_users.tokens
 
 TABLESPACE pg_default;
 
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+-- Table: tsd_users.temp_reset_keys
+
+-- DROP TABLE IF EXISTS tsd_users.temp_reset_keys;
+
+CREATE TABLE IF NOT EXISTS tsd_users.temp_reset_keys
+(
+    id SERIAL NOT NULL,
+    email character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    rand_key character varying(255) COLLATE pg_catalog."default" NOT NULL,
+	create_time timestamp without time zone DEFAULT (now() AT TIME ZONE 'utc'::text),
+    CONSTRAINT temp_reset_keys_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
