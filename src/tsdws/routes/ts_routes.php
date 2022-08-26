e095ba3d4d6c1e4c3f134714dd2b92c9989a92be
carmelocassisi <carmelocassisi@gmail.com>
2022-08-26 12:21:52 +0200

    Moved headers setting from /routes to SimpleREST setHttpHeaders function

--- src/tsdws/routes/ts_routes.php
+++ src/tsdws/routes/ts_routes.php
@@ -5,10 +5,6 @@ if (getenv("ENV") == 'development') {
 	ini_set('display_errors', 'On');
 }
 
-// set Access-Control-Allow-Origin if set on environment variables
-$ACAO = getenv("Access-Control-Allow-Origin");
-if ($ACAO) header("Access-Control-Allow-Origin: " . getenv("Access-Control-Allow-Origin"));
-
 if (strripos($_SERVER["REQUEST_URI"], "values")) {
 	
 	require_once("..".DIRECTORY_SEPARATOR."controllers".DIRECTORY_SEPARATOR."TimeseriesValuesController.php");
