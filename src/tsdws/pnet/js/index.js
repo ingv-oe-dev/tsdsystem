84ef9eaf80928d575cb7f50658edd126f910dc43
carmelocassisi <carmelocassisi@gmail.com>
2024-02-26 16:18:09 +0100

    Add-on PNet: Enable plugin icon for tree items

--- src/tsdws/pnet/js/index.js
+++ src/tsdws/pnet/js/index.js
@@ -879,6 +879,16 @@ var app = {
             if (params.station && params.station != '') URL += "&station=" + params.station;
             if (params.channel && params.channel != '') URL += "&channel=" + params.channel;
             window.open(URL, target = '_blank');
+        },
+        openPluginDialog(item) {
+            //console.log(item);
+            var querystring = "/?network="+item.net_name+"&station="+item.station_name+"&channel="+item.name;
+            $("#sideR #editing").html("<div id='pluginDialog' style='padding:2em'><h3>Available plugins</h3><ul></ul></div>");
+            for (var i=0; i<item.additional_info.plugins.length; i++) {
+                var a = '<li><a href="'+ item.additional_info.plugins[i].url + querystring + '" style="cursor:pointer; text-decoration:none" target="_blank"><b>' + item.additional_info.plugins[i].name + '</b> <u>'+ item.additional_info.plugins[i].url + querystring + '</u></a></li>';
+                $("#sideR #editing #pluginDialog ul").append(a);
+            }
+            this.openSettings(false);
         }
     },
     watch: {
